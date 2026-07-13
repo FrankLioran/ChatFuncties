8f9b3849054bea2a90c39fbba7f0c9e61ca14227
FrankLioran <frank_ammerlaan@yahoo.com>
2026-07-13 21:48:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,8 @@ with st.sidebar:
             [
                 "gemini-2.5-flash-lite",
                 "gemini-3.5-flash",
-                "gemini-3.1-flash-lite"
+                "gemini-3.1-flash-lite",
+                "Gemma 4"
             ],
             index=0
         )
